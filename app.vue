99e119ae2b722af4e3080c02d1751874fdfccb9f
Victor Otávio Ferreira <victor@ferreira.id>
2023-02-16 14:51:47 -0300

    Fix my stupidness

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+const { data: navigation } = await useAsyncData("navigation", () => {
+  return fetchContentNavigation();
+});
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <header>
+      <nav>
+        <AppNavigation :navigation-tree="navigation" />
+      </nav>
+    </header>
+
+    <main class="prose">
+      <NuxtPage />
+    </main>
   </div>
 </template>
